e3c26489f518c45d7868471c3c11bf4bbec13ead
ivandjoh <ivandi.djoh@gmail.com>
2023-04-19 14:50:07 +0800

    added dependencies

--- src/main/java/com/ivandjoh/demo/service/CountryService.java
+++ src/main/java/com/ivandjoh/demo/service/CountryService.java
@@ -1,4 +1,11 @@
 package com.ivandjoh.demo.service;
 
+import com.ivandjoh.demo.model.Country;
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public interface CountryService {
+    Mono<List<Country>> getAllCountries();
 }
